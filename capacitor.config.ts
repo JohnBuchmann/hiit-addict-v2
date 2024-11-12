89cc36f2e6fc0256f4a8e8201d46d92103e47107
John Buchmann <john@integro212.com>
2024-11-12 16:20:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hiit-addict-v2',
+  webDir: 'www'
+};
+
+export default config;
